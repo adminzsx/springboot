0420515e43e4a11e3d20ffdd912d19859999ee1b
admin <1402440863@qq.com>
2023-05-31 20:43:37 +0800

    555

--- src/main/java/com/yuling/zhang/SpringbootDeanApplication.java
+++ src/main/java/com/yuling/zhang/SpringbootDeanApplication.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class SpringbootDeanApplication {
 
     public static void main(String[] args) {
+        System.out.println();
         SpringApplication.run(SpringbootDeanApplication.class, args);
     }
 
