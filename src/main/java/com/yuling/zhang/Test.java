7f81ca7b0515b48747bea54820eb9b41973b4e31
admin <1402440863@qq.com>
2023-05-31 20:27:44 +0800

    222

--- src/main/java/com/yuling/zhang/Test.java
+++ src/main/java/com/yuling/zhang/Test.java
@@ -2,11 +2,11 @@ package com.yuling.zhang;
 
 /**
  * @author zhangshixiang
- * @date 2023-05-31 20:11
+ * @date 2023-05-31 20:27
  */
 public class Test {
-
     public static void main(String[] args) {
+        System.out.println("kkkkk");
         System.out.println("hello");
         System.out.println("hello");
     }
