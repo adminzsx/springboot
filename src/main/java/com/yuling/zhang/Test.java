5efba7d96b5513772e51c8002beab9fc5adc5598
admin <1402440863@qq.com>
2023-05-31 21:26:24 +0800

    11

--- src/main/java/com/yuling/zhang/Test.java
+++ src/main/java/com/yuling/zhang/Test.java
@@ -7,15 +7,6 @@ package com.yuling.zhang;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hhhh");
-        System.out.println("hehhhllo");
-        System.out.println("helhhhlo");
-        System.out.println("helhhhlo");
-        System.out.println("1111");
-        System.out.println("hel111lo");
-        System.out.println("hell111o");
-        System.out.println("hel111lo");
-        System.out.println("hell111o");
-        System.out.println("hel1111lo");
+        System.out.println("hello");
     }
 }
