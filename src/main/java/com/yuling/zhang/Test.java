b42293fb708524271eaa88db82f6c3aa7edb2a8a
admin <1402440863@qq.com>
2023-05-31 20:14:29 +0800

    新建test类

---
+++ src/main/java/com/yuling/zhang/Test.java
@@ -0,0 +1,12 @@
+package com.yuling.zhang;
+
+/**
+ * @author zhangshixiang
+ * @date 2023-05-31 20:11
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
